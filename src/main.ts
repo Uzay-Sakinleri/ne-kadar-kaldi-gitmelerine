f73eb5e1a11aa8bb843c081b277c51a5c959bd74
Ertan Taner <ertantaner2000@gmail.com>
2023-05-03 19:00:25 +0300

    feat: add timer page

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Türkiye'nin 20 yıldır çektiği acıların son bulmasına</h1>
+    <div id="timer"></div>
+    <hr/>
+    <h2>Eğer gerçekten bir şeylerin değişmesini istiyorsanız</h2>
+    <ol class="needs">
+    <li>Oyunuzu atmayı</li>
+    <li>Eğer fırsatınız varsa sandık müşahitliği yapmayı</li>
+    </ol>
+    <p class="dont-forget">Unutmayınız</p>
+  </div>
+`
---
+++ src/timer.ts
@@ -0,0 +1,26 @@
+
+const countDate = new Date("2023, 5, 14, 22:00:00").getTime();
+
+// Update the count down every 1 second
+const timer = setInterval(function() {
+  // Get today's date and time
+  const now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  const distance = countDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.querySelector<HTMLDivElement>("#timer")!.innerHTML = `${days} Gün ${hours} Saat ${minutes} Dakika ${seconds} Saniye Kaldı`
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(timer);
+    document.querySelector<HTMLDivElement>("demo")!.innerHTML = "20 yıllık bir ızdırap sonunda bitti, hepinize geçmiş olsun.";
+  }
+}, 1000);
